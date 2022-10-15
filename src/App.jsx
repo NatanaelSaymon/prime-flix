863571cfd75cce8077c372a5f64c7d586da97074
Natanael Saymon <saymoneo@gmail.com>
2022-10-15 10:52:58 -0300

    Preparando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>hello world!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App

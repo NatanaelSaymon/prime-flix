2f76705501381d299e0047c41c9ddc841491100a
Natanael Saymon <saymoneo@gmail.com>
2022-10-17 22:41:35 -0300

    feat: página de erro finalizada

--- src/assets/pages/Erro/index.jsx
+++ src/assets/pages/Erro/index.jsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom'
+import styles from './styles.module.css'
+
 export function Erro(){
     return(
-        <h1>Página de Erro</h1>
+        <section className={styles.erro__container}>
+            <h1>404</h1>
+            <p>Ops... essa página não existe!</p>
+            
+            <Link to="/" className={styles.btn__back}>
+                Click aqui para voltar a Home
+            </Link>
+        </section>
     )
 }
